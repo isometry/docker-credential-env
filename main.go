ba35fe56254e5d74452b3db98de149e383bded21
Robin Breathe <robin.breathe@nexthink.com>
2021-10-14 19:02:17 +0200

    Add special support for AWS ECR

--- main.go
+++ main.go
@@ -27,16 +27,24 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecr"
 )
 
 var GitCommit = ""
 var Version = "0.0.0"
 var PreRelease = "dev"
+
+var ecrHostname = regexp.MustCompile(`^[0-9]\.dkr\.ecr\.[-a-z0-9]+\.amazonaws\.com$`)
 
 const (
 	envPrefix         = "DOCKER"
@@ -89,17 +97,48 @@ func main() {
 
 // lookupEnvCredential searches the environment looking Docker registry credentials for hostname,
 // stripping least-significant DNS labels on failure
-func lookupEnvCredential(hostname string) (credential Credential, exists bool) {
+func lookupEnvCredential(hostname string) (credential Credential, found bool) {
 	labels := strings.Split(hostname, ".")
 	for i := 0; i <= len(labels); i++ {
 		envHostname := strings.Join(labels[i:], envSeparator)
 		envUsername := strings.Join([]string{envPrefix, envHostname, envUsernameSuffix}, envSeparator)
 		envPassword := strings.Join([]string{envPrefix, envHostname, envPasswordSuffix}, envSeparator)
-		if credential.Username, exists = os.LookupEnv(envUsername); exists {
-			if credential.Password, exists = os.LookupEnv(envPassword); exists {
+		if credential.Username, found = os.LookupEnv(envUsername); found {
+			if credential.Password, found = os.LookupEnv(envPassword); found {
 				break
 			}
 		}
+	}
+	if !found && ecrHostname.MatchString(hostname) {
+		// This is an AWS ECR Docker Registry: <account-id>.dkr.ecr.<region>.amazonaws.com
+		region := labels[3]
+		var err error
+		if credential, err = getEcrToken(region); err == nil {
+			found = true
+		}
+	}
+	return
+}
+
+func getEcrToken(region string) (credential Credential, err error) {
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx) // includes authentication-via-environment
+	if err != nil {
+		return
+	}
+
+	client := ecr.NewFromConfig(cfg)
+
+	output, err := client.GetAuthorizationToken(ctx, nil)
+	if err != nil {
+		return
+	}
+	for _, authData := range output.AuthorizationData {
+		// authData.AuthorizationToken is a base64-encoded username:password string,
+		// where the username is always expected to be "AWS".
+		tokenBytes, _ := base64.StdEncoding.DecodeString(*authData.AuthorizationToken)
+		token := strings.SplitN(string(tokenBytes), ":", 1)
+		credential.Username, credential.Password = token[0], token[1]
 	}
 	return
 }
